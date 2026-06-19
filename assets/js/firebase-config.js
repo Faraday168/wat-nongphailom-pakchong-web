bd568d9c1bc74355366b8d58e998976973d20bd1
Faraday168 <166583315+Faraday168@users.noreply.github.com>
2026-06-19 18:19:56 +0700

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,10 +1,21 @@
-// วัดหนองไผ่ล้อม V3 Ultimate - Firebase Config
-// ใส่ค่าจริงจาก Firebase Console > Project settings > Web app
-export const firebaseConfig = {
-  apiKey: "ใส่_apiKey",
-  authDomain: "ใส่_project.firebaseapp.com",
-  projectId: "ใส่_projectId",
-  storageBucket: "ใส่_project.appspot.com",
-  messagingSenderId: "ใส่_senderId",
-  appId: "ใส่_appId"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBpI_ytmZvQ42bksydcD_zXg6aOF8GRtUs",
+  authDomain: "wat-nongphailom-pakchong-web.firebaseapp.com",
+  projectId: "wat-nongphailom-pakchong-web",
+  storageBucket: "wat-nongphailom-pakchong-web.firebasestorage.app",
+  messagingSenderId: "1098225420650",
+  appId: "1:1098225420650:web:5f948f89040cb0ecf27d13",
+  measurementId: "G-7FVZGCBRMF"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
