29a83c7ce824f99078b751ac3345af4353e4eb26
Faraday168 <166583315+Faraday168@users.noreply.github.com>
2026-06-19 20:15:50 +0700

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBpI_ytmZvQ42bksydcD_zXg6a0F8GRtUs",
+  apiKey: "AIzaSyBpI_ytmZvQ42bksydcD_zXg6aOF8GRtUs",
   authDomain: "wat-nongphailom-pakchong-web.firebaseapp.com",
   projectId: "wat-nongphailom-pakchong-web",
   storageBucket: "wat-nongphailom-pakchong-web.firebasestorage.app",
   messagingSenderId: "1098225420650",
-  appId: "1:1098225420650:web:5f948f89040cb0ecf27d13",
-  measurementId: "G-7FVZGCBRMF"
+  appId: "1:1098225420650:web:81e59b09f4eda8d1f27d13",
+  measurementId: "G-H2NM4WCJWZ"
 };
